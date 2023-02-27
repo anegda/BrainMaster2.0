5569c3cdfd8608ba054ae87bd5b6e4a5de2be687
anegda <anegdaldama@gmail.com>
2023-02-27 19:37:07 +0100

    Login interfaz y Registro arregladas || Dialog de Fecha hecho

--- app/src/main/java/com/example/brainmaster/MainActivity.java
+++ app/src/main/java/com/example/brainmaster/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.brainmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn_reg = (Button) findViewById(R.id.btn_registrar);
+        btn_reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Registro.class));
+            }
+        });
+
+        Button btn_log = (Button) findViewById(R.id.btn_entrar);
+        btn_log.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Login.class));
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/brainmaster/Registro.java
+++ app/src/main/java/com/example/brainmaster/Registro.java
@@ -1,14 +1,40 @@
 package com.example.brainmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class Registro extends AppCompatActivity {
-
+    Calendar calendario = Calendar.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        DatePickerDialog.OnDateSetListener date =new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int anyo, int mes, int dia) {
+
+                EditText fechaN = (EditText) findViewById(R.id.fechaNacREdit);
+                fechaN.setText(Integer.toString(anyo)+"/"+Integer.toString(mes)+"/"+Integer.toString(dia));
+            }
+        };
+
+        EditText fechaN = (EditText) findViewById(R.id.fechaNacREdit);
+        fechaN.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(Registro.this,date,calendario.get(Calendar.YEAR),calendario.get(Calendar.MONTH),calendario.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
     }
 }
