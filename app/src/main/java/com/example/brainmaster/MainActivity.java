4dc5bb54688b5eb5904f7eada28cfe09bf50349c
anegda <anegdaldama@gmail.com>
2023-02-28 20:29:37 +0100

    Logo + Botón idiomas en proceso

--- app/src/main/java/com/example/brainmaster/MainActivity.java
+++ app/src/main/java/com/example/brainmaster/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.brainmaster;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,6 +36,51 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, Login.class));
             }
         });
+
+        ImageButton btn_idiomas = (ImageButton) findViewById(R.id.idiomas);
+        /*btn_idiomas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle(getString(R.string.idiomas));
+                final CharSequence[] opciones = {"Español", "English", "Euskera"};
+                builder.setSingleChoiceItems(opciones, -1, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        if(i==0){
+                            Locale nuevaloc = new Locale("en");
+                            Locale.setDefault(nuevaloc);
+                            Configuration configuration = getBaseContext().getResources().getConfiguration();
+                            configuration.setLocale(nuevaloc);
+                            configuration.setLayoutDirection(nuevaloc);
+
+                            Context context = getBaseContext().createConfigurationContext(configuration);
+                            getBaseContext().getResources().updateConfiguration(configuration, context.getResources().getDisplayMetrics());
+                        }
+                        else if(i==1){
+                            Locale nuevaloc = new Locale("es");
+                            Locale.setDefault(nuevaloc);
+                            Configuration configuration = getBaseContext().getResources().getConfiguration();
+                            configuration.setLocale(nuevaloc);
+                            configuration.setLayoutDirection(nuevaloc);
+
+                            Context context = getBaseContext().createConfigurationContext(configuration);
+                            getBaseContext().getResources().updateConfiguration(configuration, context.getResources().getDisplayMetrics());
+                        }
+                        else{
+                            Locale nuevaloc = new Locale("eu");
+                            Locale.setDefault(nuevaloc);
+                            Configuration configuration = getBaseContext().getResources().getConfiguration();
+                            configuration.setLocale(nuevaloc);
+                            configuration.setLayoutDirection(nuevaloc);
+
+                            Context context = getBaseContext().createConfigurationContext(configuration);
+                            getBaseContext().getResources().updateConfiguration(configuration, context.getResources().getDisplayMetrics());
+                        }
+                    }
+                });
+            }
+        });*/
 
     }
 }
