c61ab93ab0f3e2a094c42dda60ac7d99bed51224
anegda <anegdaldama@gmail.com>
2023-03-07 13:57:28 +0100

    EMPEZAMOS CON EL JUEGO

--- app/src/main/java/com/example/brainmaster/FragmentBotonesTablero.java
+++ app/src/main/java/com/example/brainmaster/FragmentBotonesTablero.java
@@ -1,30 +1,35 @@
 package com.example.brainmaster;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FragmentBotonesTablero#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
 public class FragmentBotonesTablero extends Fragment {
 
     public interface  listenerDelFragment{
 
     }
     private listenerDelFragment elListener;
+    static ArrayList<Integer> resultado;
 
     public FragmentBotonesTablero() {
         // Required empty public constructor
     }
 
-    public static FragmentBotonesTablero newInstance(String param1, String param2) {
+    public static FragmentBotonesTablero newInstance() {
         FragmentBotonesTablero fragment = new FragmentBotonesTablero();
         return fragment;
     }
@@ -35,8 +40,33 @@ public class FragmentBotonesTablero extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_botones_tablero, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        View.OnClickListener clicker = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Button btn = (Button) getView().findViewById(view.getId());
+                int num = Integer.parseInt((String) btn.getText());
+                resultado.add(num);
+                Log.d("DAS",Integer.toString(num));
+            }
+        };
+
+        view.findViewById(R.id.button1).setOnClickListener(clicker);
+        view.findViewById(R.id.button2).setOnClickListener(clicker);
+        view.findViewById(R.id.button3).setOnClickListener(clicker);
+        view.findViewById(R.id.button4).setOnClickListener(clicker);
+        view.findViewById(R.id.button5).setOnClickListener(clicker);
+        view.findViewById(R.id.button6).setOnClickListener(clicker);
+        view.findViewById(R.id.button7).setOnClickListener(clicker);
+        view.findViewById(R.id.button8).setOnClickListener(clicker);
+        view.findViewById(R.id.button9).setOnClickListener(clicker);
+    }
+
 }
--- app/src/main/java/com/example/brainmaster/JuegoBotonesTablero.java
+++ app/src/main/java/com/example/brainmaster/JuegoBotonesTablero.java
@@ -2,16 +2,54 @@ package com.example.brainmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.RippleDrawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.util.TypedValue;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 public class JuegoBotonesTablero extends AppCompatActivity{
+    static ArrayList<Integer> solucion;
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            ClaseBotonesJuego juego = new ClaseBotonesJuego();
+            boolean finalizar = false;
+            while(!finalizar){
+                ArrayList<Integer> ronda = juego.getSecuencia();
+                for(Integer i:ronda){
+                    int btn_id = getResources().getIdentifier("button"+Integer.toString(i),"id",getPackageName());
+                    Button btn_act = (Button) findViewById(btn_id);
 
+                    btn_act.setBackgroundColor(Color.RED);
+                    //ESPERAMOS UN SEGUNDO ANTES DE APAGARLO
+                    Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        public void run() {
+                            int btn_id = getResources().getIdentifier("button"+Integer.toString(i),"id",getPackageName());
+                            Button btn_act = (Button) findViewById(btn_id);
+                            btn_act.setBackgroundColor(getColor(R.color.purple_500));
+                        }
+                    }, 1000);
+                }
+                finalizar=true;
+            }
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_juego_botones_tablero);
+
+        Handler handler = new Handler();
+        handler.postDelayed(runnable,4000);
     }
 
 }
